3a9e53f8e2adc40a190bbae41ff1a2cfd99c5a3d
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-02-14 00:12:27 +0300

    Создан declarative_base и session

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -8,14 +8,14 @@ USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
-async def users_data_get(users_data_url: str):
+async def users_data_get(users_data_url: str) -> list[dict]:
     async with aiohttp.ClientSession() as session:
         async with session.get(users_data_url) as response:
             data_users: dict = await response.json()
             return data_users
 
 
-async def posts_data_get(posts_data_url: str):
+async def posts_data_get(posts_data_url: str) -> list[dict]:
     async with aiohttp.ClientSession() as session:
         async with session.get(posts_data_url) as response:
             data_posts: dict = await response.json()
--- homework_04/models.py
+++ homework_04/models.py
@@ -7,10 +7,27 @@
 для модели Post обязательными являются user_id, title, body
 создайте связи relationship между моделями: User.posts и Post.user
 """
-
 import os
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, declared_attr
+from sqlalchemy import Column, Integer
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
-Base = None
-Session = None
+async_engine: AsyncEngine = create_async_engine(url=PG_CONN_URI)
+
+
+class Base:
+    @declared_attr
+    def __tablename__(cls):
+        """
+        User -> blog_users
+        Author -> blog_authors
+        """
+        return f"blog_{cls.__name__.lower()}s"
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=Base)
+Session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
