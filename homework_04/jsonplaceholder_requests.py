2edf9c01722bba6d991ec6fc043e065a5a6ab818
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-02-13 22:36:23 +0300

    Сделал асинхронные запросы к jsonplaceholder

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,6 +1,32 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
+import asyncio
+import aiohttp
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+async def users_data_get(users_data_url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(users_data_url) as response:
+            data_users: dict = await response.json()
+            return data_users
+
+
+async def posts_data_get(posts_data_url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(posts_data_url) as response:
+            data_posts: dict = await response.json()
+            return data_posts
+
+
+async def main():
+    result = await users_data_get(USERS_DATA_URL)
+    result_posts = await posts_data_get(POSTS_DATA_URL)
+    return result, result_posts
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
