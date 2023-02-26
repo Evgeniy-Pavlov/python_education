20685f2b2562c7222a9eb1c5172771cd0bf6f148
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-02-26 19:05:30 +0300

    поправил создание пользователей и постов, один коммит на всех

--- homework_04/main.py
+++ homework_04/main.py
@@ -18,32 +18,28 @@ from jsonplaceholder_requests import fetch_users_data, fetch_posts_data, USERS_D
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def create_user(session: AsyncSession, name: str, username: str, email: str) -> User:
-    user = User(name=name, username=username, email=email)
-    session.add(user)
+async def create_user(session: AsyncSession, data_users) -> list[User]:
+    users = [User(name=i.get('name'), username=i.get('username'), email=i.get('email')) for i in data_users]
+    session.add_all(users)
     await session.commit()
-    return User
+    return users
 
 
-async def create_post(session: AsyncSession, user_id: int, title: str, body: str) -> Post:
-    post = Post(user_id=user_id, title=title, body=body)
-    session.add(post)
+async def create_post(session: AsyncSession, data_posts):
+    posts = [Post(user_id=i.get('userId'), title=i.get('title'), body=i.get('body')) for i in data_posts]
+    session.add_all(posts)
     await session.commit()
-    return Post
+    return posts
 
 
 async def async_main():
     await create_tables()
     result_users, result_posts = await asyncio.gather(fetch_users_data(USERS_DATA_URL),
                                                       fetch_posts_data(POSTS_DATA_URL))
-    for user in result_users:
-        async with Session() as session:
-            await create_user(session=session, name=user.get('name'),
-                              username=user.get('username'), email=user.get('email'))
-    for post_item in result_posts:
-        async with Session() as session:
-            await create_post(session=session, user_id=int(post_item.get('userId')), title=post_item.get('title'),
-                              body=post_item.get('body'))
+    async with Session() as session:
+        await create_user(session, result_users)
+    async with Session() as session:
+        await create_post(session, result_posts)
 
 
 def main():
