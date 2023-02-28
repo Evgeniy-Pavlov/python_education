6eeb969028bd3c8e4e5f8cfaaa090b0f03acd9f3
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-02-28 19:32:00 +0300

    создано приложение

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,19 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+from flask import Flask, request, render_template, flash
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def index_view():
+    return {'data': f'{datetime.now()}'}
+
+@app.get('/about/')
+def about_view():
+    return {'data': 'Hello'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
