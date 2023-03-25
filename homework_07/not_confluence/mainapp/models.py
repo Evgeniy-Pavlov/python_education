9ccbca018d09142598d703ac063938365cbbc863
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-03-25 17:11:19 +0300

    созданы миграции, таблицы зарегистрированы в админке

--- homework_07/not_confluence/mainapp/models.py
+++ homework_07/not_confluence/mainapp/models.py
@@ -2,25 +2,32 @@ from django.db import models
 import datetime
 
 # Create your models here.
-class Project(models.Model):
-    name = models.CharField(unique=True, max_length=20)
-    date_create = models.DateTimeField(default=datetime.datetime.now())
-    deleted = models.BooleanField()
-
-
-class Article(models.Model):
-    title = models.CharField(unique=True, max_length=30)
-    body = models.TextField(blank=True, null=True, max_length=3000)
-    date_create = models.DateTimeField(default=datetime.datetime.now())
-    deleted = models.BooleanField()
-    project_id = models.ForeignKey(Project.id)
-    author = models.ForeignKey(User.id)
 
 class User(models.Model):
     login = models.CharField(unique=True, max_length=10)
     last_name = models.CharField(max_length=20)
     first_name = models.CharField(max_length=20)
     patronymic = models.CharField(max_length=20)
-    date_create = models.DateTimeField(default=datetime.datetime.now())
-    deleted = models.BooleanField()
+    date_create = models.DateTimeField(null=True)
+
+
+class Project(models.Model):
+    name = models.CharField(unique=True, max_length=20)
+    user_create = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_create = models.DateTimeField(null=True) 
+    user_update = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='user_projects')
+    date_update = models.DateTimeField(null=True)
+
+
+class Article(models.Model):
+    title = models.CharField(unique=True, max_length=30)
+    body = models.TextField(blank=True, null=True, max_length=3000)
+    date_create = models.DateTimeField(null=True)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT, null=False)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user_update = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='user_articles')
+    date_update = models.DateTimeField(null=True)
+
+
+
 
